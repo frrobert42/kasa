337d4ef174c1d15371e4323cd1a90008313adf0c
Fred <robert.frederic7@gmail.com>
2024-05-19 13:28:12 +0200

    update dropdown

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -1,6 +1,7 @@
 import "./dropdown.scss";
 import PropTypes from "prop-types";
 import {useState} from "react";
+import arrow from "../../assets/img/arrow.svg";
 
 function Dropdown(props) {
     const [show, setShow] = useState(false);
@@ -8,12 +9,15 @@ function Dropdown(props) {
 
     function showContent() {
        setShow(!show);
+       const arrow = document.querySelector(".arrow" + name);
+       show ? arrow.style.transform = "rotate(0deg)" : arrow.style.transform = "rotate(180deg)";
     }
 
     return (
         <div className={"dropdown"}>
             <button onClick={() => showContent()} className={"dropdown"}>
                 <p>{name}</p>
+                <img src={arrow} alt={"arrow"} className={"arrow" + name}/>
             </button>
             {show &&
                 <p className={"dropdown-text"}>{text}</p>
