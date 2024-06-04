eba997ce1434558436cbcf3ebbf990a0b8c68258
Fred <robert.frederic7@gmail.com>
2024-06-04 14:40:54 +0200

    fix about and router :bug:

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,16 +1,9 @@
 import logoFooter from '../../assets/img/logo_footer.png';
+import './footer.scss';
+
 function Footer() {
-    let footerStyle = {
-        backgroundColor: "#000000",
-        color: "#FFFFFF",
-        textAlign: "center",
-        margin: "40px 0 0 0",
-        padding: "66px 0 29px 0",
-        width: "100%",
-        height: "140px"
-    };
     return (
-    <footer style={footerStyle}>
+    <footer>
         <img src={logoFooter} alt={"logo kasa"} width={"122"} height={"40"}/>
         <p>© 2020 Kasa. All rights reserved</p>
     </footer>
--- src/index.js
+++ src/index.js
@@ -3,35 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import {
-//     createBrowserRouter,
-//     RouterProvider,
-// } from "react-router-dom";
-// import Home from "./pages/home/home";
-// import House from "./pages/house/house";
-// import About from "./pages/about/about";
-// import NotFound from "./pages/notFound/notFound";
-
-// Create the router
-// const router = createBrowserRouter([
-//     {
-//         path: "*",
-//         element: <App />,
-//         errorElement: <NotFound />,
-//         children: [
-//             { path: "", index: true, element: <Home /> },
-//             { path: "house/:id", element: <House /> },
-//             { path: "about", element: <About /> },
-//             { path: "*", element: <NotFound /> },
-//         ],
-//     }
-// ]);
 
 // Render the router
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      {/*<RouterProvider router={router} />*/}
       <App />
   </React.StrictMode>
 );
